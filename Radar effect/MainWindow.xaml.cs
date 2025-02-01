12a6d8027f1cf95e548e193d15bbb61c8cf3c6ae
Valijon Ibroximov <valijonibroximov@gmail.com>
2025-02-02 02:04:28 +0500

    Add project files.

---
+++ Radar effect/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Radar_effect
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateRadarAnimation();
+        }
+
+        private void CreateRadarAnimation()
+        {
+            // Radar animation
+            DoubleAnimation rotationAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = 360,
+                Duration = new Duration(TimeSpan.FromSeconds(5)), // Aylanish davomiyligi
+                RepeatBehavior = RepeatBehavior.Forever // Cheksiz takrorlash
+            };
+
+            // Animatsiyani aylanish effekti bilan bog'lash
+            RotateTransform radarTransform = (RotateTransform)RadarSweep.RenderTransform;
+            radarTransform.BeginAnimation(RotateTransform.AngleProperty, rotationAnimation);
+        }
+
+        private void MenuQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
